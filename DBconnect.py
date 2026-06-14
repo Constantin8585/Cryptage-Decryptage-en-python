f6c121aeb62abbc5cc5b63dc43c99145d80ad540
GADEGBE Abed-Nego <130496884+Constantin8585@users.noreply.github.com>
2026-06-14 14:44:51 +0200

    Remove password from DB connection

--- DBconnect.py
+++ DBconnect.py
@@ -5,7 +5,7 @@ def connecter_bd():
     connexion = mysql.connector.connect(
         host="localhost",
         user="root",
-        password="8585Abed8585oK",
+        password="",
         database="crypt_decrypt_python"
     )
     return connexion
